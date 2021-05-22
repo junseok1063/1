2dca5f5af6b80736a53ca628fc62e2931d822a8a
junseok1063 <68852298+junseok1063@users.noreply.github.com>
2021-05-22 18:38:07 +0900

    Add files via upload

--- shell.c
+++ shell.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <pwd.h> 
 
 #define MAX_LEN_LINE    100
 
@@ -15,24 +16,58 @@ int main(void)
     int ret, status;
     pid_t pid, cpid;
     
+    char *user_name;
+    struct passwd *user_pw;
+
+    user_name =getlogin();
+    user_pw = getpwnam( user_name);
+    char hostname[50];
+    int host_len=50;
+    if (gethostname(hostname, host_len)==1){
+        printf("gethostname Error!\n");
+        exit(1);
+    }
+    char *cwd;
+    char wd[BUFSIZ];
+
+    cwd = getcwd(NULL, BUFSIZ);
+    
+    int cycle=0;
+    
     while (true) {
         char *s;
         int len;
-        
-        printf("junseok@DESKTOP-FH84UA5 $ ");
+        char username=user_pw->pw_name;
+        printf("%c[1;32m",27);
+        printf("%s@%s",user_pw->pw_name,hostname);
+        printf("%c[0m",27);
+        printf(":");
+        printf("%c[1;34m",27);
+        printf("~%s",cwd);
+        printf("%c[0m",27);
+        printf("$ ");
         s = fgets(command, MAX_LEN_LINE, stdin);
         if (s == NULL) {
             fprintf(stderr, "fgets failed\n");
             exit(1);
         }
-        
         len = strlen(command);
-        printf("%d\n", len);
         if (command[len - 1] == '\n') {
             command[len - 1] = '\0'; 
         }
+        if(strncmp(command, "exit", strlen(command))==0) {
+            return -1;
+        }
+        char ChangHomeDir[MAX_LEN_LINE]={"/home/%s",username};
+        if(strncmp(command, "cd",strlen(command))==0){
+           cwd=chdir(ChangeHomeDir);
+        }
+        if(strncmp(command,"cd %s"))
+      
+        printf("%d\n", len);
         
         printf("[%s]\n", command);
+
       
         pid = fork();
         if (pid < 0) {
@@ -56,6 +91,8 @@ int main(void)
                 return 1;
             }
         } 
+        cycle+=1;
+        printf("number of time :%d\n",cycle);
     }
     return 0;
 }
